31675b084c7214970bd6259d89aea8d52c07d39a
Anastasija Temova <119525418+ATemova@users.noreply.github.com>
2024-05-20 01:46:49 +0200

    Update v5.asm

--- v5.asm
+++ v5.asm
@@ -35,13 +35,80 @@ RANDOM_NUM:
     MOV D, A
     RET
 
+str_loading1: DB "Guess\x00"
+str_loading2: DB " the bigger\x00"
+str_loading3: DB " number of\x00"
+str_loading4: DB " symbols..\x00"
+start_game_str_1: DB " Press tab\x00"
+start_game_str_2: DB "to START\x00"
+
+draw_text: ;;draw the text
+    MOV C, str_loading1 ; Point to the "Guess" string. 
+    MOV B, 25           ; Set the color of the text to 25. 
+    MOV D, 0x100C       ; The VRAM position of the text. 
+    CALL draw           ; Call function to actually draw the text 
+    MOV C, str_loading2 
+    MOV D, 0x1204 
+    CALL draw 
+    MOV C, str_loading3 
+    MOV D, 0x1404 
+    CALL draw
+    MOV C, str_loading4 
+    MOV D, 0x1604 
+    CALL draw
+    MOV C, start_game_str_1 
+    MOV D, 0x1804 
+    CALL draw
+    MOV C, start_game_str_2 
+    MOV D, 0x1A04 
+    CALL draw
+    RET 
+
+draw: ;;draw function
+    MOVB BH, [C]        ; Get a character. 
+    CMPB BH, 0          ; If the character is 0, we are done. 
+    JE draw_return 
+    MOV A, D            ; Set the VRAM address for the character 
+    OUT 8               ; through the VIDADDR I/O register. 
+    MOV A, B            ; Set the character and its color 
+    OUT 9               ; through the VIDDATA I/O register. 
+    INC C               ; Point to the next character. 
+    ADD D, 2            ; Set the next VRAM address. 
+    JMP draw 
+draw_return: 
+    RET 
+
+check_press: ;; Function to check for key press
+    IN 5 ;;read the keyboard status 
+    CMP A, 0 ;;has anything happened? 
+    JE check_press ;;if not, read the keyboard status again 
+    MOV B, A ;;let the status be in register B 
+    IN 6 ;;read the key code 
+    AND B, 1 ;;mask out the keyboard bit 
+    CMP B, 1 ;;is this bit set to 1? 
+    JE start_game ;;if yes, start the game
+    JMP check_press ;;otherwise, keep checking for key press
+
+start_game:
+    MOV [QUIT], 1 ;; Set QUIT flag to 1 to exit the loading loop
+    RET
+
 MAIN:
     MOV SP, 0x0FFF
     MOV A, 1
     OUT 7
-    MOV A, 1
-    OUT 7
 
+    CALL draw_text ; Display loading text
+
+WAIT_FOR_ENTER:
+    CALL check_press
+    MOV A, [QUIT]
+    CMP A, 1
+    JNE WAIT_FOR_ENTER
+
+    MOV A, 3
+    OUT 7 ; Clear screen
+    
 ; LEVEL 1: 10 club symbols and 9 leaf symbols
 
     ; Printing 10 club symbols
