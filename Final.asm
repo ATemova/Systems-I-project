d9c21a9c6ac89ce881a3cd229b02a4809362c2f0
Anastasija Temova <119525418+ATemova@users.noreply.github.com>
2024-05-25 02:10:06 +0200

    Update Final.asm

--- Final.asm
+++ Final.asm
@@ -422,4 +422,5 @@ end_wait_loop_for_choice3:
     MOVB [SCORE], 0             ; reset SCORE to 0
     
     JMP MAIN                    ; jump to the start of MAIN
-    HLT                         ; halt the CPU, stopping program execution
+    HLT                         ; halt the CPU, stopping program executionJMP MAIN        				; jump to the MAIN section
+JMP isr         				; jump to the interrupt service routine
